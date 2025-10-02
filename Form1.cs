7f4cc56be0329e073825cb46a4f352fc766e61c8
SPdream <37748496+SPdream99@users.noreply.github.com>
2025-10-02 15:12:16 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            long sum = 0;
+            num1 = Int32.Parse(textBox1.Text);
+            num2 = Int32.Parse(textBox2.Text);
+            sum = num1 + num2;
+            textBox3.Text = sum.ToString();
+        }
+    }
+}
